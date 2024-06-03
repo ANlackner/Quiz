e9cac739f65cfe2249978c3a295bcfc7627de368
ANlackner <alackner@schueler.hakzell.at>
2024-06-03 12:23:08 +0200

    erste Änderungen in der Program.cs. einbindung der perasser.core

---
+++ quiz.cli/Program.cs
@@ -0,0 +1,13 @@
+﻿using Quiz.Core;
+
+Game testGame = new Game("Testspiel zum Testen");
+
+Player player1 = new Player("Herbert");
+Player player2 = new Player("Manfred");
+Player player3 = new Player("Rene");
+Player player4 = new Player("Thomas");
+
+
+IQuestionProvider staticProvider = new StaticQuestions();
+
+testGame.AddRound(staticProvider, true, 5);
